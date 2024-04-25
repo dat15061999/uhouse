807d464b4e8064664ec420c7ce683a536d9973da
Dat Pham <oagim1412@gmail.com>
2024-04-25 17:26:15 +0700

    'mmm'

---
+++ src/module/building/building.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { BuildingService, RoomService } from '@service';
+import { BuildingRepository, RoomRepository } from '@repository';
+@Module({
+  imports: [],
+  controllers: [],
+  providers: [BuildingRepository, RoomRepository, BuildingService, RoomService],
+  exports: [BuildingService, RoomService],
+})
+export class BuildingModule { }
